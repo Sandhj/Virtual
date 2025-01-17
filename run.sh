39e6648a94e5cb861f0cea189900919c3228cdbd
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-17 14:45:12 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-# Path absolut ke virtual environment
-VENV_PATH="/root/project/web"
-# Path absolut ke aplikasi
-APP_PATH="/root/project/app.py"
-# Menjalankan aplikasi menggunakan Python dari virtual environment dan mengalihkan output
-$VENV_PATH/bin/python $APP_PATH > /root/project/app.log 2>&1 &
+#!/bin/bash
+source /root/project/web/bin/activate
+python /root/project/app.py
