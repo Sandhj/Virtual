d6b0c5306c717bc9cd87fee42eddadf755db1b8a
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-17 13:28:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,23 +21,6 @@ VENV_DIR="$PROJECT_DIR/web"
 APP_FILE="$PROJECT_DIR/app.py"
 SERVICE_FILE="/etc/systemd/system/app.service"
 
-# Periksa apakah direktori proyek ada
-if [ ! -d "$PROJECT_DIR" ]; then
-    echo "Membuat direktori proyek di $PROJECT_DIR..."
-    mkdir -p "$PROJECT_DIR"
-fi
-
-# Buat virtual environment jika belum ada
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Membuat virtual environment di $VENV_DIR..."
-    python3 -m venv "$VENV_DIR"
-fi
-
-# Aktifkan venv dan instal dependensi
-echo "Menginstal dependensi di venv..."
-source "$VENV_DIR/bin/activate"
-pip install -r "$PROJECT_DIR/requirements.txt"
-
 # Buat file systemd service
 echo "Membuat file service di $SERVICE_FILE..."
 cat <<EOL > $SERVICE_FILE
