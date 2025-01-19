01aed8e9fdbe7611bd6ff67b2580f4f6cb18faf4
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-19 19:59:36 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -6,7 +6,7 @@ sudo apt install git
 apt install python3.11-venv
 mkdir -p xl/templates/
 cd xl
-wget -q https://raw.githubusercontent.com/Sandhj/project/main/app2.sh
+wget -q https://raw.githubusercontent.com/Sandhj/project/main/app2.py
 
 cd templates
 wget -q https://raw.githubusercontent.com/Sandhj/project/main/dashboard_xl.sh
