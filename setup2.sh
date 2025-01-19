b94e15afd1c724c03ed483c34d050a06ba9c9d03
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-19 20:01:05 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -9,8 +9,8 @@ cd xl
 wget -q https://raw.githubusercontent.com/Sandhj/project/main/app2.py
 
 cd templates
-wget -q https://raw.githubusercontent.com/Sandhj/project/main/dashboard_xl.sh
-wget -q https://raw.githubusercontent.com/Sandhj/project/main/adminadduser.sh
+wget -q https://raw.githubusercontent.com/Sandhj/project/main/templates/dashboard_xl.sh
+wget -q https://raw.githubusercontent.com/Sandhj/project/main/templates/adminadduser.sh
 
 cd
 cd xl
