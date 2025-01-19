7b1d7129fbc804f889b82dfc8c9c91447124abb8
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-19 19:40:17 +0800

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd
+apt update 
+sudo apt install git
+
+wget -q https://raw.githubusercontent.com/Sandhj/project/main/setup.sh
+
+cd project
+python3 -m venv web
+source web/bin/activate
+
+pip install flask
+pip install requests
+deactivate
+
+cd
+cat <<EOL > /etc/systemd/system/app.service
+[Unit]
+Description=Run project script
+After=network.target
+
+[Service]
+ExecStart=/bin/bash /root/project/run.sh
+WorkingDirectory=/root/project
+User=root
+Group=root
+Restart=always
+StandardOutput=journal
+StandardError=journal
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemd dan aktifkan service
+systemctl daemon-reload
+systemctl enable app.service
+
+# Menjalankan service
+systemctl start app.service
+
+echo "Setup selesai. Service app.service telah berjalan."
+
+cd
+rm setup.sh
