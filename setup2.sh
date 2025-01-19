c66c5c7c204bf57b0d3f1b000369abc9aaa47adf
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-19 19:52:33 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -11,42 +11,14 @@ cd templates
 wget -q https://raw.githubusercontent.com/Sandhj/project/main/dashboard_xl.sh
 wget -q https://raw.githubusercontent.com/Sandhj/project/main/adminadduser.sh
 
-cd project
+cd
+cd xl
 python3 -m venv web
 source web/bin/activate
 
 pip install flask
 pip install requests
 deactivate
-
-cd
-cat <<EOL > /etc/systemd/system/app.service
-[Unit]
-Description=Run project script
-After=network.target
-
-[Service]
-ExecStart=/bin/bash /root/project/run.sh
-WorkingDirectory=/root/project
-User=root
-Group=root
-Restart=always
-StandardOutput=journal
-StandardError=journal
-RestartSec=5
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-# Reload systemd dan aktifkan service
-systemctl daemon-reload
-systemctl enable app.service
-
-# Menjalankan service
-systemctl start app.service
-
-echo "Setup selesai. Service app.service telah berjalan."
 
 cd
 rm setup.sh
